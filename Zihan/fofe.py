51fa0488bd9e7a8d4e075efa0190f8c72e7bf3d0
Zihan Zheng <zizheng@ucsd.edu>
2018-05-18 10:39:56 -0700

    Add files via upload

---
+++ Zihan/fofe.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class FofeVectorizer():
+
+    def __init__( self, alpha = 0.99 ):
+        self.alpha = alpha
+
+    def naive_transform( self, docs, vocabulary ):
+        x = np.zeros(( len( docs ), len( vocabulary )))
+        for row_i, doc in enumerate( docs ):
+            for word in doc:
+                x[row_i,:] *= self.alpha
+                try:
+                    col_i = vocabulary[word]
+                except KeyError:
+                    # not in vocabulary: a one-letter word or a word from a test set
+                    continue
+                x[row_i, col_i] += 1
+        return x
